ba0b1884edd18207404c9f11ec0aca0aa326f131
vojtechlevi <91605543+vojtechlevi@users.noreply.github.com>
2021-12-02 10:16:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner myScanner = new Scanner(System.in);
+        Random rand = new Random(6 + 1);
+
+        System.out.println(rand);
+
+
+
+
+
+    }
+}
